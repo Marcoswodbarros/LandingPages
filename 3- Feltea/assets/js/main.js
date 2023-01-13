fc6553c73caf6bc2b9d6f75b0251718ebfa116ad
Marcos Barros <108278189+Marcoswodbarros@users.noreply.github.com>
2023-01-13 17:13:05 -0300

    Updating the js
    - JS;
    - Inserting the buttons.

--- 3- Feltea/assets/js/main.js
+++ 3- Feltea/assets/js/main.js
@@ -1,15 +1,21 @@
 let section = document.querySelector('#ingredients');
 let obs = document.querySelector('.ingredients__obs');
 let sections = document.querySelector('.ingredients__container');
+let arrowDown = document.querySelector('.fa-circle-chevron-down');
+let arrowUp = document.querySelector('.fa-circle-chevron-up');
 
-obs.addEventListener('click', showMenu);
-sections.addEventListener('click', hideMenu);
+arrowDown.addEventListener('click', showMenu);
+arrowUp.addEventListener('click', hideMenu);
 
 
 function showMenu () {
+    arrowDown.style.display = 'none';
+    arrowUp.style.display = 'flex';
     sections.style.display = 'flex';
 }
 
 function hideMenu () {
+    arrowDown.style.display = 'flex';
+    arrowUp.style.display = 'none'
     sections.style.display = 'none';
 }
