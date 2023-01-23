cee18f70d0fa3bdc1c2d04144493538579a04e1a
Marcos Barros <108278189+Marcoswodbarros@users.noreply.github.com>
2023-01-23 12:27:59 -0300

    Updating the js

--- 5- Picland/assets/js/main.js
+++ 5- Picland/assets/js/main.js
@@ -1,18 +1,41 @@
-let arrowDown = document.querySelector('.fa-circle-chevron-down');
-let arrowUp = document.querySelector('.fa-circle-chevron-up');
-let cameras = document.querySelector('.section__img--container');
+let arrowDown = document.querySelector('.camera__down');
+let arrowUp = document.querySelector('.camera__up');
+let forestDown = document.querySelector('.forest__down');
+let forestUp = document.querySelector('.forest__up');
+let cameras = document.querySelector('.section__img--cameras');
+let forests = document.querySelector('.section__img--forests');
 
-arrowDown.addEventListener('click', show);
-arrowUp.addEventListener('click', hide);
+arrowDown.addEventListener('click', showCamera);
+arrowUp.addEventListener('click', hideCamera);
 
-function show () {
+forestDown.addEventListener('click', showForest);
+forestUp.addEventListener('click', hideForest);
+
+
+
+// Functions
+
+function showCamera () {
     arrowDown.style.display = "none";
     arrowUp.style.display = "block";
     cameras.style.display = "block";
 }
 
-function hide () {
+function hideCamera () {
     arrowDown.style.display = "block";
     arrowUp.style.display = "none";
     cameras.style.display = "none";
+}
+
+
+function showForest () {
+    forestDown.style.display = "none";
+    forestUp.style.display = "block";
+    forests.style.display = "block";
+}
+
+function hideForest () {
+    forestDown.style.display = "block";
+    forestUp.style.display = "none";
+    forests.style.display = "none";
 }
